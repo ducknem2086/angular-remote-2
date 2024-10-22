5c84c0b930088f747829d6042f79e9828d8fef36
nem <hihihehehd1198>
2024-10-22 18:58:17 +0700

    update package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-remote-2';
+  title = 'angular-remote-21231231skjdhfgjkshdf';
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
-  name: 'angular-remote-2',
+  name: "angular-remote-2",
 
   exposes: {
-    './EntityRoute':'./src/app/pages/entity/entity.routing.ts'
+    "./EntityRoute": "./src/app/pages/entity/entity.routing.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
